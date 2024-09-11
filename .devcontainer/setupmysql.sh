8a7df8847bfe8bee4f7a595ba96242eae03dc182
Nigel Edwards <nigel.edwards@aru.ac.uk>
2024-09-11 14:24:48 +0000

    again

--- .devcontainer/setupmysql.sh
+++ .devcontainer/setupmysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install mysql-client -y
 docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=mydb -p 3306:3306 -d mysql:latest
-apt update
-apt install mysql-client -y
+
 mysql --version
