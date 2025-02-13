ec8ac524525342b7a593d1986404c8ab1786b33e
Nigel Edwards <nigel.edwards@aru.ac.uk>
2025-02-13 14:39:11 +0000

    testing

--- .devcontainer/setupcrashcoursemysql.sh
+++ .devcontainer/setupcrashcoursemysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword -e "CREATE DATABASE crashcourse"
-mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ./.devcontainer/create.sql
-mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ./.devcontainer/populate.sql
+mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/create.sql
+mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/populate.sql
--- .devcontainer/setupmysql.sh
+++ .devcontainer/setupmysql.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install mysql-client -y
 docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=mydb -p 3306:3306 -d mysql:latest
 
+sleep 5
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword -e "CREATE DATABASE crashcourse"
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/create.sql
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/populate.sql
