25dd3f0edf5742eca668ef4c231ee92110a0aa31
Nigel Edwards <nigel.edwards@aru.ac.uk>
2025-02-13 14:50:55 +0000

    testing

--- .devcontainer/setupmysql.sh
+++ .devcontainer/setupmysql.sh
@@ -4,7 +4,9 @@ sudo apt update
 sudo apt install mysql-client -y
 docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=mydb -p 3306:3306 -d mysql:latest
 
+echo "############################################# Nigel is sleeping ############################################################"
 sleep 5
+echo "############################################# Nigel is AWAKE :-) ###########################################################"
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword -e "CREATE DATABASE crashcourse"
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/create.sql
 mysql -h 127.0.0.1 -P 3306 -u root -prootpassword < ${CODESPACE_VSCODE_FOLDER}/.devcontainer/populate.sql
